e9a16ad43ac49056b3b0654639cb1bdcba213935
gyl30 <im.ggyyll@gmail.com>
2025-10-18 17:23:55 +0800

    feat(ui): 添加应用程序图标

--- main.cpp
+++ main.cpp
@@ -1,7 +1,25 @@
 #include <QApplication>
+#include <QPixmap>
+#include <QPainter>
+#include <QFont>
+#include <QIcon>
 #include "log.h"
 #include "mainwindow.h"
 #include "scoped_exit.h"
+
+static QIcon emoji_to_icon(const QString &emoji, int size)
+{
+    QPixmap pixmap(size, size);
+    pixmap.fill(Qt::transparent);
+    QPainter painter(&pixmap);
+    auto font = QApplication::font();
+    font.setPointSizeF(size * 0.8);
+    painter.setFont(font);
+    painter.setPen(Qt::black);
+    painter.drawText(pixmap.rect(), Qt::AlignCenter, emoji);
+    painter.end();
+    return pixmap;
+}
 
 int main(int argc, char* argv[])
 {
@@ -11,6 +29,7 @@ int main(int argc, char* argv[])
 
     QApplication app(argc, argv);
     mainwindow main_window;
+    QApplication::setWindowIcon(emoji_to_icon("♫ ", 64));
     main_window.show();
     return app.exec();
 }
